b776a01e25843365d60a89fd3a7a83bfe669b36a
Toby Matejovsky <toby.matejovsky@gmail.com>
2010-11-01 03:54:22 -0400

    Fix 'case' syntax for ruby 1.9, fix bad encoding

--- lib/readability.rb
+++ lib/readability.rb
@@ -183,13 +183,13 @@ module Readability
     def score_node(elem)
       content_score = class_weight(elem)
       case elem.name.downcase
-        when "div":
+        when "div"
           content_score += 5
-        when "blockquote":
+        when "blockquote"
           content_score += 3
-        when "form":
+        when "form"
           content_score -= 3
-        when "th":
+        when "th"
           content_score -= 5
       end
       { :content_score => content_score, :elem => elem }
@@ -277,7 +277,7 @@ module Readability
       end
 
       # Get rid of duplicate whitespace
-      node.to_html.gsub(/[\r\n\f]+/, "\n" ).gsub(/[\t  ]+/, " ")
+      node.to_html.gsub(/[\r\n\f]+/, "\n" ).gsub(/[\t  ]+/, " ")
     end
 
     def clean_conditionally(node, candidates, selector)
