6b11246670cb4ec6cfc004ad893a98a853583af0
Ricky Gu <rickyzgu@gmail.com>
2012-04-27 21:49:38 -0700

    handle image tag with empty src value.

--- lib/readability.rb
+++ lib/readability.rb
@@ -71,8 +71,7 @@ module Readability
       elements = content.css("img").map(&:attributes)
 
         elements.each do |element|
-          next unless element
-          url     = URI.escape(element["src"].value)
+          url     = URI.escape(element["src"].value) if element["src"].value
           height  = element["height"].nil?  ? 0 : element["height"].value.to_i
           width   = element["width"].nil?   ? 0 : element["width"].value.to_i
           format  = File.extname(url).gsub(".", "")
