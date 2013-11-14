edeb0a44e8fa1427be4377e014cdd80c199cc584
John Pagonis <john@pagonis.org>
2013-11-14 15:25:25 +0000

    Fixes #30 and also adds API to make images FQDN in the resulting readable content.

--- lib/readability.rb
+++ lib/readability.rb
@@ -117,6 +117,31 @@ module Readability
 
       (list_images.empty? and content != @html) ? images(@html, true) : list_images
     end
+    
+    def images_with_fqdn_uris!(source_uri)
+      images_with_fqdn_uris(@html, source_uri)
+    end
+    
+    def images_with_fqdn_uris(document = @html.dup, source_uri)
+      uri = URI.parse(source_uri)
+      host = uri.host
+      scheme = uri.scheme
+      port = uri.port # defaults to 80
+
+      base = "#{scheme}://#{host}:#{port}/"
+
+      images = []
+      document.css("img").each do |elem|
+        begin
+          elem['src'] = URI.join(base,elem['src']).to_s if URI.parse(elem['src']).host == nil 
+          images << elem['src'].to_s
+        rescue URI::InvalidURIError => exc
+          elem.remove
+        end
+      end
+
+      images(document,true)
+    end
 
     def get_image_size(url)
       begin
